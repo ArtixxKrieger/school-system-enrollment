835857ae447294f3f80a01d0e9995bf6fb96fff0
ArtixxKrieger <114236039+ArtixxKrieger@users.noreply.github.com>
2026-05-20 01:52:47 +0000

    Remove large generated files from git tracking

--- .migration-backup/lib/db/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./users.js";
-export * from "./courses.js";
-export * from "./enrollees.js";
-export * from "./students.js";
-export * from "./curriculum.js";
-export * from "./settings.js";
-export * from "./vouchers.js";
